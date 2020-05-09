33125f43986acaabe90055456fd62b3bd0812a15
Yevhenii Veretennikov <veret.evg@gmail.com>
2020-05-09 21:06:35 +0900

    Initial Commit

---
+++ CollectionX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionX
+//
+//  Created by Yevhenii on 09.05.2020.
+//  Copyright © 2020 Yevhenii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
