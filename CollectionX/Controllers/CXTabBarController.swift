0b4d35661ed559cc0054865bee0defad3c778089
Yevhenii Veretennikov <veret.evg@gmail.com>
2020-06-09 12:21:24 +0900

    Leave only 'Search' and new 'Library' tabs.

--- CollectionX/Controllers/CXTabBarController.swift
+++ CollectionX/Controllers/CXTabBarController.swift
@@ -10,11 +10,8 @@ import UIKit
 
 class CXTabBarController: UITabBarController {
 
-    private var archiveNavController   : UINavigationController!
-    private var bookmarkNavController  : UINavigationController!
     private var searchNavController    : UINavigationController!
-    private var favoritesNavController : UINavigationController!
-    private var settingsNavController  : UINavigationController!
+    private var libraryNavController   : UINavigationController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,36 +19,21 @@ class CXTabBarController: UITabBarController {
     }
 
     private func setup() {
-        archiveNavController    = createTabController(controller: ArchiveViewController(),
-                                                      title: "Archive",
-                                                      icon: SFSymbols.archiveBoxFill,
-                                                      tag: 0)
-        bookmarkNavController   = createTabController(controller: BookmarkViewController(),
-                                                      title: "To Watch",
-                                                      icon: SFSymbols.bookmarkFill,
-                                                      tag: 1)
         searchNavController     = createTabController(controller: SearchViewController(),
                                                       title: "Search",
                                                       icon: SFSymbols.magnifyingGlass,
                                                       tag: 2)
-        favoritesNavController  = createTabController(controller: FavoritesViewController(),
-                                                      title: "Favorites",
-                                                      icon: SFSymbols.starFill,
+        libraryNavController  = createTabController(controller: FavoritesViewController(),
+                                                      title: "Library",
+                                                      icon: SFSymbols.cards,
                                                       tag: 3)
-        settingsNavController   = createTabController(controller: SettingsViewController(),
-                                                      title: "Settings",
-                                                      icon: SFSymbols.gear,
-                                                      tag: 4)
 
         viewControllers = [
-            archiveNavController,
-            bookmarkNavController,
             searchNavController,
-            favoritesNavController,
-            settingsNavController
+            libraryNavController
         ]
 
-        selectedIndex = 3
+        selectedIndex = 1
     }
 
     private func createTabController<T: UIViewController>(controller: T, title: String?, icon: UIImage?, tag: Int) -> UINavigationController {
